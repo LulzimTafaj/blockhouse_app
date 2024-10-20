61c3465f626c0c314dd2cc7f982068ac4c86d71d
lui <lui@pop-os.localdomain>
2024-10-20 02:44:23 -0400

    Modified urls

--- blockhouse/urls.py
+++ blockhouse/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('stocks/', include('stocks.urls'))
+    path('', RedirectView.as_view(url='/stocks/', permanent=False)),  # Redirect root URL to /stocks/
+    path('stocks/', include('stocks.urls')),  # Ensure your stocks URLs are included
 ]
